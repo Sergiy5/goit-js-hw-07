90d02a9f61f85c9e5c0174fc1087d77d3b1abc79
Sergiy Kushnir <s1983kushnir@gmail.com>
2023-02-18 19:45:39 +0200

    1 dun

---
+++ js/01-gallery.js
@@ -0,0 +1,51 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+
+    function makeLinkGalleryItem(galleryItems) {
+      return galleryItems
+        .map(({ preview, original, description }) => {
+          return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+        })
+        .join("");
+};
+
+const containerOfGallery = document.querySelector(".gallery");
+const galleryMarkup = makeLinkGalleryItem(galleryItems);
+
+containerOfGallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+containerOfGallery.addEventListener("click", onlinkClick);
+
+function onlinkClick(evt) {
+  // Заборонити стандаотні дії
+    evt.preventDefault();
+    
+  if (evt.target.nodeName !== "IMG") {
+    return;
+    }
+    
+  // Відкриття бібліотеки basicLightbox та шлях до джерела.
+    const instance = basicLightbox.create(
+        `<img src="${evt.target.dataset.source}">`);
+    
+    instance.show();
+    
+    // Закриття бібліотеки
+    containerOfGallery.addEventListener('keydown', (evt) => {
+        if (evt.code === 'Escape') {
+            instance.close();
+        }
+    });
+};
