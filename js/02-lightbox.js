f4f4c8e7031380e74abbd69089f3ffa722a9e776
Sergiy Kushnir <s1983kushnir@gmail.com>
2023-02-19 14:22:13 +0200

    2 dun

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+function makeLinkGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__item" href="${original}" >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+    `;
+    })
+    .join("");
+}
+
+const listOfGallery = document.querySelector(".gallery");
+const galleryMarkup = makeLinkGalleryItem(galleryItems);
+
+listOfGallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// SimpleLightbox
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+
+
+
+
